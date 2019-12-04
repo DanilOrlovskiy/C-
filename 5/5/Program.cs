165056d3aa81caf2efc0d7afaee5f677e51f2c62
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:48:51 +0200

    задание 5

---
+++ 5/5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(input == 1 ? "Понедельник" : input == 2 ? "Вторник" : input == 3 ? "Среда" :
+                input == 4 ? "Четверг" : input == 5 ? "Пятница" : input == 6 ? "Суббота" : input == 7 ? "Воскресенье" : "Такого дня недели не существует");
+           
+
+        }
+    }
+}
