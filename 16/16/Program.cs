3e2831c0b2a33d2802657f133553d70209482649
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:51:32 +0200

    Задание 16

---
+++ 16/16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = "root";
+            do
+            {
+                Console.WriteLine("Введите пароль.\n");
+                string inputPass = Convert.ToString(Console.ReadLine());
+                if (pass == inputPass)
+                {
+                    Console.WriteLine("Пароль верный!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nПароль не верный! Попробуйте еще.\n");
+                }
+            }
+            while (true);
+        }
+    }
+}
