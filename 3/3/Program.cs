5871af210bb314a02bce4e6a56602b5fa3726df8
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:48:30 +0200

    Задание 3

---
+++ 3/3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if((input == 1) || (input == 2) || (input == 12)){
+                Console.WriteLine("Зима");
+            }
+            else if ((input == 3) || (input == 4) || (input == 5)){
+                Console.WriteLine("Весна");
+            }
+            else if ((input == 6) || (input == 7) || (input == 8)){
+                Console.WriteLine("Лето");
+            }
+            else if ((input == 9) || (input == 10) || (input == 11)){
+                Console.WriteLine("Осень");
+            }
+            else {
+                Console.WriteLine("На этой планете такого месяца - нет");
+            }
+
+        }
+    }
+}
