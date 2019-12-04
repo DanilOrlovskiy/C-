6f2bb4904484199a01be8c62cfc02bbe6200caf8
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:51:14 +0200

    Задание 14

---
+++ 14/14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int a = rnd.Next(0, 100);
+            int b = rnd.Next(0, 100);
+            int c = rnd.Next(0, 100);
+            int d = rnd.Next(0, 100);
+            int e = rnd.Next(0, 100);
+
+            Console.WriteLine("Число 1:  " + a);
+            Console.WriteLine("Число 2:  " + b);
+            Console.WriteLine("Число 3:  " + c);
+            Console.WriteLine("Число 4:  " + d);
+            Console.WriteLine("Число 5:  " + e);
+
+            Console.WriteLine("\nСреднее арифметическое этих чисел " + ((a + b + c + d + e) / 5));
+
+        }
+    }
+}
