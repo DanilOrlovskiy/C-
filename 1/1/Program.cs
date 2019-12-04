1608076bcf60a3715f75013335504ba704c23f55
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:46:52 +0200

    Задание 1

---
+++ 1/1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nВведите число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b){
+                Console.WriteLine("Значение" + a + " - Больше");
+            }
+            else if (a == b){
+                Console.WriteLine("a = b");
+            }
+            else{
+                Console.WriteLine("Значение " + b + " - Больше");
+            }
+            
+        }
+    }
+}
