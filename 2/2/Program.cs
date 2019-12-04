f697b12556bcf4fdd9cf72d73cbc41674ce5a4fe
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:47:54 +0200

    Задание 2

---
+++ 2/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myaw = "Мяу";
+            string gav = "Гав";
+            string input = Console.ReadLine();
+            if (input == myaw)
+            {
+                Console.WriteLine("Покорми кота");
+            }
+            else if (input == gav)
+            {
+                Console.WriteLine("Погуляй с собакой");
+            }
+            else
+            {
+                Console.WriteLine("Неправильно");
+            }
+        }
+    }
+}
