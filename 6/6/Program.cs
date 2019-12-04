02dc7e8c765e3cc134f62be87c50b524f1f16f1b
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:49:02 +0200

    Задание 6

---
+++ 6/6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какое растояние вы проехали (в км)?");
+            int km = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Количество минут простоя?");
+            int time = Convert.ToInt32(Console.ReadLine());
+            if(km<=5){
+
+                Console.WriteLine("Сумма оплаты:" + (20 + time));
+            }
+             else{
+                Console.WriteLine("Сумма оплаты:" + (((km-5)*3) + (20 + time)));
+            }
+        }
+    }
+}
