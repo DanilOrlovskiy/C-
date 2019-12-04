f07d0a563094915f1dc18fa54f8180f2e20ace64
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:50:53 +0200

    Задание 12

---
+++ 12/12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, x;
+            Console.WriteLine("Введите число N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            i = 1;
+            x = i * i;
+            while (x <= n)
+            {
+                Console.WriteLine(x);
+                i++;
+                x = i * i;
+            }
+        }
+    }
+}
