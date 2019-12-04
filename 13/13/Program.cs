c9722dcf9f663e6083c7fd258406eb4b49bc7680
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:51:02 +0200

    Задание 13

---
+++ 13/13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            int e = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Среднее арифметическое этих чисел " + ((a+b+c+d+e)/5));
+                     
+        }
+    }
+}
