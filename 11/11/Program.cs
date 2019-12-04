a03726bdd20bae1b47262f58be0f09e24743112e
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:50:45 +0200

    Задание 11

---
+++ 11/11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(1, 146);
+            while(true)
+            {
+                Console.WriteLine("Введите число");
+                int value2 = Convert.ToInt32(Console.ReadLine());
+
+                if (value == value2)
+                {
+                    Console.WriteLine("Вы угадали, " + value + " - загаданное число");
+                    break;
+                }
+                else if (value < value2)
+                {
+                    Console.WriteLine("Вы не угадали, загаданное число Меньше");
+                }
+                else
+                {
+                    Console.WriteLine("Вы не угадали, загаданное число Больше");
+                }
+            }
+            Console.WriteLine("Конец");
+        }
+    }
+}
