085c33743ab6666da7a0a366a31aab4d89e6b881
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:48:40 +0200

    Задание 4

---
+++ 4/4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(input == 1 ? "Хорошо" : "Плохо");
+        }
+    }
+}
