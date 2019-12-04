d062b8784cf3045ec236991125933223876f1a78
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:49:23 +0200

    Задание 7

---
+++ 7/7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input > 1)
+            {
+                for(int i=2; i < input; i++)
+                {
+                    if (input % i == 0)
+                    {
+                        Console.WriteLine("Число не простое");
+                    }
+                    
+                }
+                Console.WriteLine("Число простое");
+            }
+            else
+            {
+                Console.WriteLine("Число не простое");
+            }
+
+        }
+    }
+}
