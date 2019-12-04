a416843bab2cfc421ec68a334de218f509100974
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-04 22:50:22 +0200

    Задание 10

---
+++ 10.1/10.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+            n /= 10;
+            while(n>0)
+            {
+                n /= 10;
+                count++;
+            }
+            Console.WriteLine("Количество разрядов: " + count);
+        }
+    }
+}
---
+++ 10/10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N");
+            int val = Convert.ToInt32(Console.ReadLine());
+            int c;
+            for (int i = 1; i < 21; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}\n", val, i, val * i);
+            }
+        }
+    }
+}
