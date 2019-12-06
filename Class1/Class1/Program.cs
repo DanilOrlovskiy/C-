9b4cc2ada8b7eda13313b2f278a394f5794a6418
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-06 22:04:22 +0200

    Done

---
+++ Class1/Class1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Program1
+{
+    class Adress
+    {
+        string index;
+        public string Index
+        {
+            set
+            {
+                index = value;
+            }
+            get
+            {
+                return index;
+            }
+        }
+        private string country;
+        public string Country
+        {
+            set
+            {
+                country = value;
+            }
+            get
+            {
+                return country;
+            }
+        }
+        string city;
+        public string City
+        {
+            set
+            {
+                city = value;
+            }
+            get
+            {
+                return city;
+            }
+        }
+        string street;
+        public string Street
+        {
+            set
+            {
+                street = value;
+            }
+            get
+            {
+                return street;
+            }
+        }
+        string house;
+        public string House
+        {
+            set
+            {
+                house = value;
+            }
+            get
+            {
+                return house;
+            }
+        }
+        string apartment;
+        public string Apartment
+        {
+            set
+            {
+                apartment = value;
+            }
+            get
+            {
+                return apartment;
+            }
+        }
+    }
+        class Program
+        {
+            static void Main()
+            {
+
+            Adress program = new Adress();
+
+            program.Index = "49128";
+            Console.WriteLine(program.Index);
+            program.Country = "Украина";
+            Console.WriteLine(program.Country);
+            program.City = "Днепр";
+            Console.WriteLine(program.City);
+            program.Street = "Красный камень";
+            Console.WriteLine(program.Street);
+            program.House = "3";
+            Console.WriteLine(program.House);
+            program.Apartment = "80";
+            Console.WriteLine(program.Apartment);
+
+            }
+        }
+}
---
+++ Classes/Class2/Class2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Class2
+{
+    class Rectangle
+    {
+        double side1;
+        double side2;
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        public double AreaCalculator()
+        {
+            return side1 + side2;
+        }
+        public double PerimeterCalculator()
+        {
+            return 2*(side1 + side2);
+        }
+
+        public double Area
+        {
+            get { return AreaCalculator(); }
+        }
+
+        public double Perimeter
+        {
+            get { return PerimeterCalculator(); }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Rectangle(25, 66);
+            Console.WriteLine("Area: {0}, Perimeter: {1}", program.Area, program.Perimeter);
+        }
+    }
+}
