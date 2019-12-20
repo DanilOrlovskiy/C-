3bdee0d9608a378787be6faca4ee8ce0d53827fb
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2019-12-20 23:32:59 +0200

    done

---
+++ Classes/Сlass8/Сlass8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Class8
+{
+    class Invoice
+    {
+        public int account;
+        public string customer;
+        public  string provider;
+        private string article;
+        private int quantity;
+
+        public Invoice(int account, string customer, string provider, string article, int quantity)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+            this.article = article;
+            this.quantity = quantity;
+        }
+        public double Price()
+        {
+            account *= quantity;
+            return account;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(10000, "ООО 'Покупатель' ", "ООО 'Поставщик'", "Компьютер", 2);
+            Console.WriteLine("Покупатель: {0}\nПродавец: {1}\nСумма заказа с НДС: {2}\nСумма заказа без НДС: {3}\n", invoice.customer, invoice.provider,invoice.account, invoice.account*0.2+invoice.account);
+            Console.ReadKey();
+        }
+    }
+}
