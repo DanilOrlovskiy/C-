2d3d8371880242eae6707dd8c19144714df95d37
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2020-01-02 16:32:57 +0200

    1

---
+++ Classes/Class7/Class7/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Class7
+{
+    class Employee
+    {
+        public string name;
+        public string surname;
+        public static string dateOfHire;
+        public OperateCost operationCost;
+
+        public Employee(string name, string surname, string dateOfHire, OperateCost opCost)
+        {
+            this.name = name;
+            this.surname = surname;
+            Employee.dateOfHire = dateOfHire;
+            this.operationCost = opCost;
+
+        }
+
+        public static double DiscoverGrade(string dateOfHire)
+        {
+            double dateValueForGrade = (DateTime.Now - DateTime.Parse(dateOfHire)).TotalDays;
+
+            if (dateValueForGrade >= 1825 && dateValueForGrade < 3650)
+                return 1.1;
+            else if (dateValueForGrade >= 3650)
+                return 1.25;
+            else
+                return 1.5;
+        }
+    }
+        abstract class OperateCost
+        {
+            public double salary;
+            public double tax;
+
+            public abstract void ApplyBonus(double bonus, double grade);
+            public abstract void ApplyTax();
+        }
+
+        class Junior : OperateCost
+        {
+            public override void ApplyBonus(double bonus, double grade)
+            {
+                salary = (10000 + 2 * bonus) * grade;
+            }
+
+            public override void ApplyTax()
+            {
+                tax = salary * 0.25;
+            }
+        }
+
+        class Middle : OperateCost
+        {
+            public override void ApplyBonus(double bonus, double grade)
+            {
+                salary = (25000 + 5 * bonus) * grade;
+            }
+            public override void ApplyTax()
+            {
+                tax = salary * 0.25;
+            }
+        }
+
+        class Senior : OperateCost
+        {
+            public override void ApplyBonus(double bonus, double grade)
+            {
+                salary = (50000 + 10 * bonus) * grade;
+            }
+            public override void ApplyTax()
+            {
+                tax = salary * 0.25;
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                OperateCost oc = new Middle();
+                Employee emp = new Employee("Данил", "Орловский", "12.12.2019", oc);
+                double grade = Employee.DiscoverGrade("09.02.2000");
+                Console.WriteLine(" Имя: {0}\n Фамилия: {1}\n Дата приема на работу: {2}\n Должность: {3}\n", emp.name, emp.surname, Employee.dateOfHire, emp.operationCost);
+                oc.ApplyBonus(10000, grade);
+                oc.ApplyTax();
+                Console.WriteLine("Зарплата: {0}, Налог: {1}", emp.operationCost.salary, emp.operationCost.tax);
+                Console.ReadKey();
+            }
+        }
+}
---
+++ inheritance/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class Printer
+    {
+        public virtual void Print(string value)
+        {
+            Console.WriteLine("\n"+ value);
+        }
+    }
+
+    class Red : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            base.Print(value);
+        }
+    }
+    class Blue : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            base.Print(value);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer print = new Printer();
+            print.Print("Первая строка");
+            print = new Red();
+            print.Print("Вторая строка");
+            print = new Blue();
+            print.Print("Третья строка");
+
+            Console.ReadKey();
+        }
+    }
+}
