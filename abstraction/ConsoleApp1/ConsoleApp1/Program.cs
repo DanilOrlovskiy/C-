107f4e4b8eea6443283e699329fd6a1a6d178213
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2020-01-06 14:34:40 +0200

    Abstraction Done

---
+++ abstraction/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    abstract class AbstractHandler
+    {
+       public abstract void Open();
+       public abstract void Create();
+       public abstract void Change();
+       public abstract void Save();
+    }
+    class XMLHandler : AbstractHandler
+    {
+       public override void Open()
+        {
+            Console.WriteLine("Открыть XML");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("Создать XML");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("Изменить XML");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Сохранить XML");
+        }
+    }
+    class TXTHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Открыть TXT");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("Создать TXT");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("Изменить TXT");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Сохранить TXT");
+        }
+    }
+    class DOCHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Открыть DOC");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("Создать DOC");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("Изменить DOC");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Сохранить DOC");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string formatDoc;
+            Console.WriteLine("Введите формат документа");
+            formatDoc = Console.ReadLine();
+            if(formatDoc == "xml")
+            {
+                XMLHandler xml = new XMLHandler();
+                xml.Open();
+                xml.Create();
+                xml.Change();
+                xml.Save();
+            }
+           else if (formatDoc == "txt")
+            {
+                TXTHandler txt = new TXTHandler();
+                txt.Open();
+                txt.Create();
+                txt.Change();
+                txt.Save();
+            }
+            else if (formatDoc == "doc")
+            {
+                DOCHandler doc = new DOCHandler();
+                doc.Open();
+                doc.Create();
+                doc.Change();
+                doc.Save();
+            }
+        }
+    }
+}
---
+++ abstraction/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+    interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+    class Player : IPlayable , IRecodable
+    {
+     void IPlayable.Play()
+        {
+            Console.WriteLine("Play");
+        }
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Pause");
+        }
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Stop");
+        }
+        void IRecodable.Record()
+        {
+            Console.WriteLine("Record");
+        }
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("Pause");
+        }
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("Stop");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPlayable playable = new Player();
+            Console.WriteLine("\n");
+            playable.Play();
+            playable.Pause();
+            playable.Stop();
+            Console.WriteLine("\n");
+            IRecodable recordable = new Player();
+            recordable.Record();
+            recordable.Pause();
+            recordable.Stop();
+        }
+    }
+}
