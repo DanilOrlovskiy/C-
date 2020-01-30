d5edbb658a669489c3faa88daf9d394268d78954
DanilOrlovskiy <57774446+DanilOrlovskiy@users.noreply.github.com>
2020-01-31 00:05:21 +0200

    Done

---
+++ Static/1/1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _1
+{
+    static class Calculator
+    {
+        public static int firstNum { get; set; }
+        public static int secondNum { get; set; }
+        public static int GetSum()
+        {
+
+            return firstNum + secondNum;
+        }
+        public static int GetSub()
+        {
+            return firstNum - secondNum;
+        }
+        public static int GetMult()
+        {
+            return firstNum * secondNum;
+        }
+        public static int GetDiv()
+        {
+            return firstNum / secondNum;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result=0;
+            int a, b;
+            Console.WriteLine("Введите два числа");
+            Calculator.firstNum = int.Parse(Console.ReadLine());
+            Calculator.secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выбор:\n 1 - прибавить\n 2 - отнять\n 3 - умножить\n 4 - разделить");
+            int menu = int.Parse(Console.ReadLine());
+            switch (menu)
+            {
+                case 1:
+                    Console.WriteLine("\nРезультат: {0}",Calculator.GetSum());
+                    break;
+                case 2:
+                    Console.WriteLine("\nРезультат: {0}", Calculator.GetSub());
+                    break;
+                case 3:
+                    Console.WriteLine("\nРезультат: {0}", Calculator.GetMult());
+                    break;
+                case 4:
+                    Console.WriteLine("\nРезультат: {0}", Calculator.GetDiv());
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Static/2/2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _2
+{
+    static class Sorting
+    {
+        public static void GetSorting(int[] array, int n)
+        {
+            int temp;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("\n Массив");
+            for (int i = 0; i < n; i++) 
+            {
+                Console.Write(array[i] + " ");
+            }
+            return;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            Console.WriteLine("Введите массив");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(" {0}-й элемент: ", i);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Sorting.GetSorting(array, n);
+        }
+    }
+}
